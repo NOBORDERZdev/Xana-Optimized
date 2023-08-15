3d205071800f9e0ccfeb2d751d7deca37d67fdaf
ArqamNoBorderz <arqam.javed@noborderz.com>
2023-08-15 12:06:43 +0500

    Update xana lobby with toggle and xana lobby bundle also uploaded

--- Assets/Scripts/HassanSohail/AI/AiAppearance.cs
+++ Assets/Scripts/HassanSohail/AI/AiAppearance.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace XanaAi
 {
     public class AiAppearance : MonoBehaviour
     {
+        public Toggle wearableToggle;
         [Header("Wearable items")]
         [SerializeField] public List<string> Uppers;
         [SerializeField] public List<string> Lower;
@@ -17,6 +19,7 @@ namespace XanaAi
         [SerializeField] public List<string> EyeLashes;
         private PhotonAIController pac;
         private int rand = 0;
+        private bool isDownloadWearables = true;
 
         private void Awake()
         {
@@ -26,18 +29,22 @@ namespace XanaAi
         public IEnumerator GetAppearance(AiController ai)
         {
             yield return new WaitForSeconds(0.2f);
-            rand = Random.Range(0, Uppers.Count);
-            pac.StartCoroutine(pac.DownloadAddressableWearableWearable( Uppers[rand], "Chest", ai));
-            Uppers.RemoveAt(rand);
-            rand = Random.Range(0, Lower.Count);
-            pac.StartCoroutine(pac.DownloadAddressableWearableWearable( Lower[rand], "Legs", ai));
-            Lower.RemoveAt(rand);
-            rand = Random.Range(0, Hair.Count);
-            pac.StartCoroutine(pac.DownloadAddressableWearableWearable( Hair[rand], "Hair", ai));
-            Hair.RemoveAt(rand);
-            rand = Random.Range(0, Shoes.Count);
-            pac.StartCoroutine(pac.DownloadAddressableWearableWearable( Shoes[rand], "Feet", ai));
-            Shoes.RemoveAt(rand);
+
+            if (isDownloadWearables)
+            {
+                rand = Random.Range(0, Uppers.Count);
+                pac.StartCoroutine(pac.DownloadAddressableWearableWearable(Uppers[rand], "Chest", ai));
+                Uppers.RemoveAt(rand);
+                rand = Random.Range(0, Lower.Count);
+                pac.StartCoroutine(pac.DownloadAddressableWearableWearable(Lower[rand], "Legs", ai));
+                Lower.RemoveAt(rand);
+                rand = Random.Range(0, Hair.Count);
+                pac.StartCoroutine(pac.DownloadAddressableWearableWearable(Hair[rand], "Hair", ai));
+                Hair.RemoveAt(rand);
+                rand = Random.Range(0, Shoes.Count);
+                pac.StartCoroutine(pac.DownloadAddressableWearableWearable(Shoes[rand], "Feet", ai));
+                Shoes.RemoveAt(rand);
+            }
 
             ai.isPerformingAction = false;
             if (ai.ActionCoroutine !=null)
@@ -45,6 +52,11 @@ namespace XanaAi
                 ai.StopCoroutine(ai.ActionCoroutine);
             }
             ai.ActionCoroutine =  ai.StartCoroutine( ai.PerformAction());
+        }
+
+        public void UpdateWearableStatus()
+        {
+            isDownloadWearables = wearableToggle.isOn;
         }
 
 
