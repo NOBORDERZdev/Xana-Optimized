c6c67cdb3e70dec7059135688527d13716a7288f
Abdullah Rashid <abdullah.rashid@noborderz.com>
2023-08-31 14:03:27 +0500

    Ai Exceptions

--- Assets/Scripts/Abdullah Rashid Scripts/FootStepsScripts/StepsManager.cs
+++ Assets/Scripts/Abdullah Rashid Scripts/FootStepsScripts/StepsManager.cs
@@ -5,56 +5,58 @@
     {
     public AudioSource StepAudio;
     public float StepsVolume = 0.07f;
-     
+    public bool isplayer = false;
     public AudioClip[] sandclips, stoneClips, waterClips, metalClips, snowClips, wetClips, floorClips = default;
     [Tooltip("Scale of the dust and track particles")]
 
     void Awake()
     {
-        StepAudio.spatialBlend = 1;
+        if(!StepAudio)
         StepAudio.volume = StepsVolume;
 
     }
-     public void EnterStep()
+    public void EnterStep()
+    {
+        if (isplayer)
         {
-           if (Physics.Raycast(gameObject.transform.position, Vector3.down, out RaycastHit footRay, 3))
+            if (Physics.Raycast(gameObject.transform.position, Vector3.down, out RaycastHit footRay, 3))
             {
-                if (StepAudio  && ReferrencesForDynamicMuseum.instance.playerControllerNew._IsGrounded)
+                if (StepAudio && ReferrencesForDynamicMuseum.instance.playerControllerNew._IsGrounded)
                 {
                     switch (footRay.collider.tag)
                     {
 
-                    case "Footsteps/sand":
-                        StepAudio.PlayOneShot(sandclips[UnityEngine.Random.Range(0, sandclips.Length - 1)]);
-                        break;
-                    case "Footsteps/stone":
-                        StepAudio.PlayOneShot(stoneClips[UnityEngine.Random.Range(0, stoneClips.Length - 1)]);
-                        break;
-                    case "Footsteps/water":
-                        StepAudio.PlayOneShot(waterClips[UnityEngine.Random.Range(0, waterClips.Length - 1)]);
-                        break;
-                    case "Footsteps/metal":
-                        StepAudio.PlayOneShot(metalClips[UnityEngine.Random.Range(0, metalClips.Length - 1)]);
-                        break;
-                    case "Footsteps/snow":
-                        StepAudio.PlayOneShot(snowClips[UnityEngine.Random.Range(0, snowClips.Length - 1)]);
-                        break;
-                    case "Footsteps/wet":
-                        StepAudio.PlayOneShot(wetClips[UnityEngine.Random.Range(0, wetClips.Length - 1)]);
-                        break;
-                    case "Footsteps/floor":
-                        StepAudio.PlayOneShot(floorClips[UnityEngine.Random.Range(0, floorClips.Length - 1)]);
-                        break;
-                    default:
-                        StepAudio.PlayOneShot(floorClips[UnityEngine.Random.Range(0, floorClips.Length - 1)]);
-                        break;
+                        case "Footsteps/sand":
+                            StepAudio.PlayOneShot(sandclips[UnityEngine.Random.Range(0, sandclips.Length - 1)]);
+                            break;
+                        case "Footsteps/stone":
+                            StepAudio.PlayOneShot(stoneClips[UnityEngine.Random.Range(0, stoneClips.Length - 1)]);
+                            break;
+                        case "Footsteps/water":
+                            StepAudio.PlayOneShot(waterClips[UnityEngine.Random.Range(0, waterClips.Length - 1)]);
+                            break;
+                        case "Footsteps/metal":
+                            StepAudio.PlayOneShot(metalClips[UnityEngine.Random.Range(0, metalClips.Length - 1)]);
+                            break;
+                        case "Footsteps/snow":
+                            StepAudio.PlayOneShot(snowClips[UnityEngine.Random.Range(0, snowClips.Length - 1)]);
+                            break;
+                        case "Footsteps/wet":
+                            StepAudio.PlayOneShot(wetClips[UnityEngine.Random.Range(0, wetClips.Length - 1)]);
+                            break;
+                        case "Footsteps/floor":
+                            StepAudio.PlayOneShot(floorClips[UnityEngine.Random.Range(0, floorClips.Length - 1)]);
+                            break;
+                        default:
+                            StepAudio.PlayOneShot(floorClips[UnityEngine.Random.Range(0, floorClips.Length - 1)]);
+                            break;
+                    }
                 }
-                }
-              
+
             }
-           
+
         }
-      
+    }
       
     }
 
