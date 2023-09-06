d99b8d24d0013bc309309607e8824ed893037940
Riken Sonani <87807293+rikensonaniNB@users.noreply.github.com>
2023-09-06 14:59:44 +0530

    fighting costume in multiplayer

---
+++ Assets/Scripts/FightingModule/XanaFightingPlayer.cs
@@ -0,0 +1,47 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XanaFightingPlayer : MonoBehaviour
+{
+    public AvatarController avatarController;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+    private void OnTransformParentChanged()
+    {
+        if (transform.parent.GetComponent<ControlsScript>())
+        {
+            if (transform.parent.GetComponent<ControlsScript>().playerNum == 1)
+            {
+                if (PhotonNetwork.LocalPlayer.ActorNumber == 1)
+                {
+                    avatarController.staticClothJson = FightingGameManager.instance.player1Data.clothJson;
+                }
+                else
+                {
+                    avatarController.staticClothJson = FightingGameManager.instance.player2Data.clothJson;
+                }
+            }
+            else
+            {
+                if (PhotonNetwork.LocalPlayer.ActorNumber == 2)
+                {
+                    avatarController.staticClothJson = FightingGameManager.instance.player2Data.clothJson;
+                }
+                else
+                {
+                    avatarController.staticClothJson = FightingGameManager.instance.player1Data.clothJson;
+                }
+            }
+            avatarController.OnEnable();
+        }
+    }
+}
