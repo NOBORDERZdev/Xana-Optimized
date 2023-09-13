5bc58faca036b6f74283a9afb5c3d536c55f799f
Hassan Sohail <107843990+HassanSohail000@users.noreply.github.com>
2023-09-13 16:07:58 +0500

    update

--- Assets/Scripts/ClothesCustomization for Multiplayer/Scripts/CharcterBodyParts.cs
+++ Assets/Scripts/ClothesCustomization for Multiplayer/Scripts/CharcterBodyParts.cs
@@ -1490,10 +1490,29 @@ public class CharcterBodyParts : MonoBehaviour
 
 
     /// <summary>
-    /// To hide player meshes
+    /// To hide player meshes for camera man account
     /// </summary>
     public void HidePlayer(){ 
+        if(Body)
         Body.gameObject.SetActive(false);
+         if(Head)
+        Head.gameObject.SetActive(false);
+         if(avatarController.wornHair)
+        avatarController.wornHair.SetActive(false);
+         if(avatarController.wornPant)
+        avatarController.wornPant.SetActive(false);
+         if(avatarController.wornShirt)
+        avatarController.wornShirt.SetActive(false);
+         if(avatarController.wornShose)
+        avatarController.wornShose.SetActive(false);
+         if(avatarController.wornEyewearable)
+        avatarController.wornEyewearable.SetActive(false);
+        if(avatarController.wornGloves)
+        avatarController.wornGloves.SetActive(false);
+         if(avatarController.wornChain)
+        avatarController.wornChain.SetActive(false);
+
+        avatarController.GetComponent<ArrowManager>().PhotonUserName.gameObject.SetActive(false);
         //.gameObject.SetActive(false);
     }
 
