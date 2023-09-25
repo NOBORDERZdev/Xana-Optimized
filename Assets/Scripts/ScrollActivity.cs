ea178e116d7e577a4554c171a4a4b63618c4b4c5
Hassan Sohail <107843990+HassanSohail000@users.noreply.github.com>
2023-09-25 12:40:25 +0500

    bounce update

--- Assets/Scripts/ScrollActivity.cs
+++ Assets/Scripts/ScrollActivity.cs
@@ -100,7 +100,7 @@ public class ScrollActivity : MonoBehaviour
     public void WaitForOpenWorldPage()
     {
         //ScrollController.transform.DOPunchPosition();
-       DOTween.To(() => ScrollController.verticalNormalizedPosition, x => ScrollController.verticalNormalizedPosition = x, 0.95f, 0.1f).SetEase(Ease.InSine).OnComplete(BounceBack);
+       DOTween.To(() => ScrollController.verticalNormalizedPosition, x => ScrollController.verticalNormalizedPosition = x, 0.97f, 0.1f).SetEase(Ease.InSine).OnComplete(BounceBack);
 
     }
 
