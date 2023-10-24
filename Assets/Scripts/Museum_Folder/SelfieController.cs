60531dadb29027bf9ad48289fe8baaaee707c51f
ArqamNoBorderz <arqam.javed@noborderz.com>
2023-10-24 11:47:03 +0500

    Update render texture data with optimization

--- Assets/Scripts/Museum_Folder/SelfieController.cs
+++ Assets/Scripts/Museum_Folder/SelfieController.cs
@@ -607,7 +607,7 @@ public class SelfieController : MonoBehaviour
 
     public void TakeScreenShootAndSaveToGallary()
     {
-        Texture2D l_Texture2d = new Texture2D(m_RenderTexture.width, m_RenderTexture.height, TextureFormat.RGB24, false);
+        Texture2D l_Texture2d = new Texture2D(m_RenderTexture.width, m_RenderTexture.height, TextureFormat.RGB24, false);  // RGB24
         RenderTexture.active = m_RenderTexture;
         l_Texture2d.ReadPixels(new Rect(0, 0, m_RenderTexture.width, m_RenderTexture.height), 0, 0);
         l_Texture2d.Apply();
@@ -618,6 +618,12 @@ public class SelfieController : MonoBehaviour
 
         m_Texture2D = l_Texture2d;
         m_CapturedImage.texture = m_Texture2D;
+
+        // optimize the render texture data     // AR changes start
+        m_RenderTexture.Release();
+        m_RenderTexture = null;
+        Resources.UnloadUnusedAssets();
+        GC.Collect();                           // AR changes end
     }
 
     public int picCount;
