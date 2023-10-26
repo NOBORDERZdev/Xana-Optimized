847ccc9862314ef7aec65fe6740cb5f5d82ea5e8
WaheedNBdev1 <145003658+WaheedNBdev1@users.noreply.github.com>
2023-10-26 13:53:59 +0500

    UI Button Animations

--- Assets/AdvancedInputField/Scripts/NativeKeyboard/ButtonAnimationScript.cs
+++ Assets/AdvancedInputField/Scripts/NativeKeyboard/ButtonAnimationScript.cs
@@ -7,7 +7,6 @@ using DG.Tweening;
     public class ButtonAnimationScript : MonoBehaviour
     {
     public RectTransform ButtonReferencetobeAnimated;
-    //[HideInInspector]
     public Vector3 CurrentPos;
     public Vector3 MovingPosition;
     public GameObject ButtontoMove;
@@ -15,7 +14,7 @@ using DG.Tweening;
         // Start is called before the first frame update
         void Start()
         {
-        //CurrentPositionofY();
+        
         }
 
         // Update is called once per frame
@@ -26,21 +25,13 @@ using DG.Tweening;
 
         public void moveButtonUp()
         {
-        //Vector3 pos = ButtonReferencetobeAnimated.transform.position;
-        //pos.y += 350f;
         ButtonReferencetobeAnimated.DOLocalMoveY(MovingPosition.y, 0.2f);
-
         }
 
         public void moveButtonDown()
         {
         ButtonReferencetobeAnimated.DOLocalMoveY(CurrentPos.y, 0.2f);
         }
-
-    public void CurrentPositionofY() 
-    {
-        CurrentPos = ButtonReferencetobeAnimated.transform.position;
-    }
 
     }
 
