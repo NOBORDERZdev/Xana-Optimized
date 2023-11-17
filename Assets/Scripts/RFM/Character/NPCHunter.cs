92fa2cc80e1e674b78f020d72eebc2d2f281422f
Muneeb Ullah <muhammad.muneeb@noborderz.com>
2023-11-17 13:41:32 +0500

    NPCs system re-work 1

--- Assets/Scripts/RFM/Character/NPCHunter.cs
+++ Assets/Scripts/RFM/Character/NPCHunter.cs
@@ -1,14 +1,12 @@
-using System;
 using System.Collections.Generic;
 using Photon.Pun;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 using Random = UnityEngine.Random;
 
 namespace RFM.Character
 {
-    public class NPCHunter : MonoBehaviour
+    public class NPCHunter : MonoBehaviour, IPunObservable
     {
         [SerializeField] private Transform cameraPosition;
         [SerializeField] private GameObject killVFX;
@@ -20,6 +18,9 @@ namespace RFM.Character
         private float _maxSpeed;
         private List<GameObject> _players;
         private Transform _target;
+        private Vector3 _targetPosition;
+
+        private bool _hasTarget = false;
 
         public Transform CameraTarget => cameraPosition;
 
@@ -37,11 +38,18 @@ namespace RFM.Character
         private void OnEnable()
         {
             EventsManager.onGameTimeup += GameOver;
+            EventsManager.onTakePositionTimeStart += OnTakePositionTimeStart;
+        }
+
+        private void OnTakePositionTimeStart()
+        {
+            GetAllRunners();
         }
 
         private void OnDisable()
         {
             EventsManager.onGameTimeup -= GameOver;
+            EventsManager.onTakePositionTimeStart -= OnTakePositionTimeStart;
         }
 
         private void Start()
@@ -51,32 +59,87 @@ namespace RFM.Character
             InvokeRepeating(nameof(SearchForTarget), 1, 1);
         }
 
+        private void GetAllRunners()
+        {
+            _players = new List<GameObject>(
+                               GameObject.FindGameObjectsWithTag(Globals.LOCAL_PLAYER_TAG));
+            _players.AddRange(new List<GameObject>(
+                               GameObject.FindGameObjectsWithTag(Globals.RUNNER_NPC_TAG)));
+        }
+
         private void SearchForTarget()
         {
-            if (_target) return;
+            if (_hasTarget) return;
 
-            _players = new List<GameObject>(
-                GameObject.FindGameObjectsWithTag(Globals.LOCAL_PLAYER_TAG));
-            _players.AddRange(new List<GameObject>(
-                GameObject.FindGameObjectsWithTag(Globals.RUNNER_NPC_TAG)));
+            //_players = new List<GameObject>(
+            //    GameObject.FindGameObjectsWithTag(Globals.LOCAL_PLAYER_TAG));
+            //_players.AddRange(new List<GameObject>(
+            //    GameObject.FindGameObjectsWithTag(Globals.RUNNER_NPC_TAG)));
 
             if (_players.Count > 0)
             {
                 _target = _players[Random.Range(0, _players.Count)].transform;
-                FollowTarget(_target.position);
+                _hasTarget = true;
+                //FollowTarget(_target.position);
             }
             else
             {
+                _hasTarget = false;
                 _navMeshAgent.isStopped = true;
+            }
+        }
+
+        private void ControlBotMovement()
+        {
+            // Bot movement logic goes here. For example:
+            if (!_hasTarget) return;
+
+            if (Globals.gameState != Globals.GameState.Gameplay)
+            {
+                _navMeshAgent.isStopped = true;
+                return;
+            }
+
+            _targetPosition = _target.position;
+            _navMeshAgent.SetDestination(_targetPosition);
+            _navMeshAgent.isStopped = false;
+
+            // Handle collision, interactions, etc.
+        }
+
+        private void SyncMovement()
+        {
+            if (!_hasTarget) return;
+
+            //if (!isMoving)
+            //{
+            //    // If the bot is supposed to be stationary, snap to the target position
+            //    agent.Warp(_target.position);
+            //}
+            //else
+            {
+                // If the bot is moving, smoothly interpolate to the target position
+                _navMeshAgent.SetDestination(_targetPosition);
             }
         }
 
         private void Update()
         {
-            if (_target)
+            // Only the master client controls the bot
+            if (PhotonNetwork.IsMasterClient)
             {
-                FollowTarget(_target.position);
+                ControlBotMovement();
             }
+            else
+            {
+                // Synchronize movement for non-master clients
+                SyncMovement();
+            }
+
+            //if (_target)
+            //{
+            //    FollowTarget(_target.position);
+            //}
 
             Vector3 velocity = _navMeshAgent.velocity;
             Vector2 velocityDir = new Vector2(velocity.x, velocity.z);
@@ -92,26 +155,26 @@ namespace RFM.Character
             npcAnim.SetFloat(velocityNameY, animVector.y);
 
 
-            // Catch player if in range of a sphere of radius = catchRadius
-            if (!RFMDevmodeManager.instance.devMode)
-            {
-                _inRangePlayer = CheckPlayerInRange();
+            if (RFM.Globals.DevMode) return;
 
-                if (_inRangePlayer == null)
+
+            // Catch player if in range of a sphere of radius = catchRadius
+            _inRangePlayer = CheckPlayerInRange();
+
+            if (_inRangePlayer == null)
+            {
+                _catchTimer = 0;
+            }
+            else
+            {
+                _catchTimer += Time.deltaTime;
+                if (_catchTimer >= timeToCatchRunner)
                 {
                     _catchTimer = 0;
-                }
-                else
-                {
-                    _catchTimer += Time.deltaTime;
-                    if (_catchTimer >= timeToCatchRunner)
-                    {
-                        _catchTimer = 0;
-                        _players.Remove(_inRangePlayer);
-                        _target = null;
-                        killVFX.SetActive(true);
-                        _inRangePlayer.GetComponent<PlayerRunner>()?.PlayerRunnerCaught(this);
-                    }
+                    _players.Remove(_inRangePlayer);
+                    _hasTarget = false;
+                    killVFX.SetActive(true);
+                    _inRangePlayer.GetComponent<PlayerRunner>()?.PlayerRunnerCaught(this);
                 }
             }
         }
@@ -136,38 +199,38 @@ namespace RFM.Character
         }
 
 
-        private void FollowTarget(Vector3 targetPosition)
-        {
-            if (Globals.gameState != Globals.GameState.Gameplay)
-            {
-                _navMeshAgent.isStopped = true;
-                return;
-            }
+        //private void FollowTarget(Vector3 targetPosition)
+        //{
+        //    if (Globals.gameState != Globals.GameState.Gameplay)
+        //    {
+        //        _navMeshAgent.isStopped = true;
+        //        return;
+        //    }
 
-            _navMeshAgent.SetDestination(targetPosition);
-            _navMeshAgent.isStopped = false;
-        }
+        //    _navMeshAgent.SetDestination(targetPosition);
+        //    _navMeshAgent.isStopped = false;
+        //}
 
         private void OnTriggerEnter(Collider other)
         {
-            if (!RFMDevmodeManager.instance.devMode)
+            if (RFM.Globals.DevMode) return;
+            if (!PhotonNetwork.IsMasterClient) return;
+
+            if (other.CompareTag(Globals.RUNNER_NPC_TAG))
             {
-                if (other.CompareTag(Globals.RUNNER_NPC_TAG))
-                {
-                    _players.Remove(other.gameObject);
-                    _target = null;
-                    killVFX.SetActive(true);
-                    other.transform.parent.GetComponent<NPCRunner>().AIRunnerCaught();
-                }
+                _players.Remove(other.gameObject);
+                _hasTarget = false;
+                killVFX.SetActive(true);
+                other.transform.parent.GetComponent<NPCRunner>().AIRunnerCaught();
+            }
 
-                else if (other.CompareTag(Globals.PLAYER_TAG)/*Globals.LOCAL_PLAYER_TAG*/)
-                {
-                    _players.Remove(other.gameObject);
-                    _target = null;
-                    killVFX.SetActive(true);
+            else if (other.CompareTag(Globals.PLAYER_TAG))
+            {
+                _players.Remove(other.gameObject);
+                _hasTarget = false;
+                killVFX.SetActive(true);
 
-                    other.GetComponent<PlayerRunner>()?.PlayerRunnerCaught(this);
-                }
+                other.GetComponent<PlayerRunner>()?.PlayerRunnerCaught(this);
             }
         }
 
@@ -176,5 +239,27 @@ namespace RFM.Character
             PhotonNetwork.Destroy(gameObject);
         }
 
+
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+            if (stream.IsWriting)
+            {
+                // Master client sends data
+                stream.SendNext(_navMeshAgent.destination);
+            }
+            else
+            {
+                // Other clients receive data
+                _targetPosition = (Vector3)stream.ReceiveNext();
+
+                // Check for discrepancies and lag compensation
+                if (Vector3.Distance(_navMeshAgent.destination, _targetPosition) > 1.0f)
+                {
+                    _navMeshAgent.SetDestination(_targetPosition);
+                }
+
+                // Additional security and validation checks can be implemented here
+            }
+        }
     }
 }
