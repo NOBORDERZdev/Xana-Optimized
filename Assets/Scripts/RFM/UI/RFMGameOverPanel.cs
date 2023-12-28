307c997bde123f894bf95ce3d024ae9816c19fc3
Muneeb Ullah <muhammad.muneeb@noborderz.com>
2023-12-28 18:36:10 +0500

    Ranking screen changed

--- Assets/Scripts/RFM/UI/RFMGameOverPanel.cs
+++ Assets/Scripts/RFM/UI/RFMGameOverPanel.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class RFMGameOverPanel : MonoBehaviour
 {
-    [SerializeField] private GameObject runnersSelectionImage;
-    [SerializeField] private GameObject huntersSelectionImage;
+    [SerializeField] private UnityEngine.UI.Image runnersSelectionImage;
+    [SerializeField] private UnityEngine.UI.Image huntersSelectionImage;
 
     [SerializeField] private GameObject runnerLeaderBoard;
     [SerializeField] private GameObject hunterLeaderBoard;
@@ -15,16 +15,16 @@ public class RFMGameOverPanel : MonoBehaviour
 
     public void RunnersButtonClicked()
     {
-        runnersSelectionImage.SetActive(true);
-        huntersSelectionImage.SetActive(false);
+        runnersSelectionImage.color = new Color(1f, 0.212f, 0.827f, 1f);
+        huntersSelectionImage.color = Color.black;
         runnerLeaderBoard.SetActive(true);
         hunterLeaderBoard.SetActive(false);
     }
 
     public void HuntersButtonClicked()
     {
-        huntersSelectionImage.SetActive(true);
-        runnersSelectionImage.SetActive(false);
+        huntersSelectionImage.color = new Color(1f, 0.212f, 0.827f, 1f);
+        runnersSelectionImage.color = Color.black;
         hunterLeaderBoard.SetActive(true);
         runnerLeaderBoard.SetActive(false);
     }
