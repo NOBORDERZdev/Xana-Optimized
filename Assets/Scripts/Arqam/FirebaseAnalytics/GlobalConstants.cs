29221b256526cc90e45ab9197549d8c39ce319a6
Ali Hamza <alihamzaunity@noborderz.com>
2023-12-28 19:21:43 +0500

    Resolve Conflicts After merge in release

--- Assets/Scripts/Arqam/FirebaseAnalytics/GlobalConstants.cs
+++ Assets/Scripts/Arqam/FirebaseAnalytics/GlobalConstants.cs
@@ -34,7 +34,29 @@ public class GlobalConstants
         CL_IMG_ZoneX,
         CL_IMG_FiveElements, // Done
         URL_ZoneX, 
-        URL_FiveElements
+        URL_FiveElements,
+
+            //Events For PMY Module
+        Home_Thumbnail_PMY,
+        Home_Thumbnail_PlayBtn_PMY,
+
+        StayTime_PMYLobby,
+        StayTime_CRoom1,
+        StayTime_CRoom2,
+        StayTime_PMYGallery,
+
+        Corporate_Room,
+        Gallery,
+
+        CL_NFT_PMYLobby,
+        CL_NFT_CRoom1,
+        CL_NFT_CRoom2,
+        CL_NFT_Gallery,
+
+        URL_PMYLobby,
+        URL_CRoom1,
+        URL_CRoom2,
+        URL_Gallery
     }
 
     public static void SendFirebaseEvent(string eventName)
