ab143fc6b91cd685fba583cf0644d8f7c3adc22b
ArqamNoBorderz <arqam.javed@noborderz.com>
2024-01-04 14:00:08 +0500

    roomA nft interactibility issue fixed

---
+++ Assets/Scripts/Arqam/PMY/RoomA_Nft_Interactbility.cs
@@ -0,0 +1,47 @@
+using PMY;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class RoomA_Nft_Interactbility : MonoBehaviour
+{
+    [Tooltip("Retrieve the index of the NFT for enabling buttons interactivity upon clicking.")]
+    public int nftIndex;
+    [Space(5)]
+    public Button[] btns;
+
+ 
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(0.5f);
+        btns = GetComponentsInChildren<Button>();
+        foreach (Button btn in btns)
+            btn.interactable = false;
+    }
+
+    private void OnEnable()
+    {
+        PMY_Nft_Manager.Instance.exitClickedAction += NftExitBtnClicked;
+    }
+
+
+    private void OnDisable()
+    {
+        PMY_Nft_Manager.Instance.exitClickedAction -= NftExitBtnClicked;
+    }
+
+    private void NftExitBtnClicked(int num)
+    {
+        if (num == nftIndex)
+        {
+            foreach (Button btn in btns)
+                btn.interactable = true;
+
+            PMY_Nft_Manager.Instance.exitClickedAction -= NftExitBtnClicked;
+        }
+    }
+
+}
