de7da22c8e97dfb0a31f04f8d0a16aae037562dc
Abdullah Rashid <abdullah.rashid@noborderz.com>
2024-01-26 16:27:33 +0500

    Update CharacterOnScreenNameHandler.cs

--- Assets/Scripts/SannanZahid/CharacterOnScreenNameHandler.cs
+++ Assets/Scripts/SannanZahid/CharacterOnScreenNameHandler.cs
@@ -38,13 +38,13 @@ public class CharacterOnScreenNameHandler : MonoBehaviour
             yield return new WaitForSeconds(1f);
             if (PlayerPrefs.GetInt("IsLoggedIn") == 0)
             {
-                Debug.LogError("SetName 1");
+              
 
                 _onScreenName.text = PlayerPrefs.GetString(ConstantsGod.GUSTEUSERNAME);
             }
             else
             {
-                Debug.LogError("SetName 2");
+               // Debug.LogError("SetName 2");
 
                 StartCoroutine(IERequestGetUserDetails());
             }
@@ -52,7 +52,7 @@ public class CharacterOnScreenNameHandler : MonoBehaviour
         else
         {
            
-            Debug.LogError("SetName");
+          
             yield return new WaitForSeconds(1f);
             if (PlayerPrefs.GetInt("IsLoggedIn") == 0)
             {
@@ -62,7 +62,7 @@ public class CharacterOnScreenNameHandler : MonoBehaviour
             }
             else
             {
-                Debug.LogError("SetName 2");
+               
                 StartCoroutine(IERequestGetUserDetails());
 
             }
