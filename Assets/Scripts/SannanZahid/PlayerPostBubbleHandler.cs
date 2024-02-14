6df8c0d5cb51a05407535f376b3a1364185fe4b8
Imran khan swati <157481994+Imran-Swati@users.noreply.github.com>
2024-02-14 19:26:58 +0500

    Dynamic Friends Post Bubble

--- Assets/Scripts/SannanZahid/PlayerPostBubbleHandler.cs
+++ Assets/Scripts/SannanZahid/PlayerPostBubbleHandler.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using UnityEngine;
 
 public class PlayerPostBubbleHandler : MonoBehaviour
@@ -11,13 +12,21 @@ public class PlayerPostBubbleHandler : MonoBehaviour
     public void InitObj(Transform _bubbleObjF, TMPro.TMP_Text postTextF)
     {
         BubbleObj = _bubbleObjF;
-        _postText = postTextF;
+
+        if (postTextF.text.Length >= 20)
+        {
+            _postText.text = InsertNewlines(postTextF.text);
+        }
+        else
+        {
+            _postText = postTextF;
+        }
     }
 
     public void ActivatePostFirendBubble(bool flag)
     {
         // Debug.LogError("ActivatePostFirendBubble -----> " + flag);
-        if (BubbleObj!=null && BubbleObj.transform.childCount>0)
+        if (BubbleObj != null && BubbleObj.transform.childCount > 0)
         {
             BubbleObj.GetChild(0).gameObject.SetActive(flag);
         }
@@ -25,8 +34,8 @@ public class PlayerPostBubbleHandler : MonoBehaviour
     void Update()
     {
         BubbleObj.position = Vector3.MoveTowards(BubbleObj.position, transform.position + Offset, 0.5f);
-       // BubbleObj.rotation = Quaternion.Slerp(BubbleObj.rotation,
-       //     Quaternion.LookRotation(_cameraTransform.position - BubbleObj.position), 
+        // BubbleObj.rotation = Quaternion.Slerp(BubbleObj.rotation,
+        //     Quaternion.LookRotation(_cameraTransform.position - BubbleObj.position), 
         //    Time.deltaTime);
 
 
@@ -34,6 +43,30 @@ public class PlayerPostBubbleHandler : MonoBehaviour
     }
     public void UpdateText(string txt)
     {
-        _postText.text = txt;
+        if (txt.Length >= 20)
+        {
+            _postText.text = InsertNewlines(txt);
+        }
+        else
+        {
+            _postText.text = txt;
+        }
+    }
+
+    public string InsertNewlines(string input)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            stringBuilder.Append(input[i]);
+
+            if ((i + 1) % 20 == 0)
+            {
+                stringBuilder.Append("\n");
+            }
+        }
+
+        return stringBuilder.ToString();
     }
 }
