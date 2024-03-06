a2c96f5b31bc402f916733f72b85a6a70da89b42
umer-aftab <umeraftab@noborderz.com>
2024-03-06 21:26:28 +0500

    exception handling of post in user profile
    exception handling of post in user profile

--- Assets/Scripts/SNS Behaviour/MyProfileDataManager.cs
+++ Assets/Scripts/SNS Behaviour/MyProfileDataManager.cs
@@ -901,8 +901,17 @@ public class MyProfileDataManager : MonoBehaviour
                 {
                     //allPhotoContainer
                     //int index = loadedMyPostAndVideoId.FindIndex(value => value == currentPageAllTextPostWithUserIdRoot.data.rows[i].id);
-                    allPhotoContainer.transform.GetChild(i).GetComponent<FeedData>().SetFeedPrefab(currentPageAllTextPostWithUserIdRoot.data.rows[i], false);
-                    allPhotoContainer.transform.GetChild(i).name = "User Feed Post old one " + i;
+                    Debug.Log("countsss" + allPhotoContainer.transform.childCount + " " + i);
+                    try
+                    {
+                        if (allPhotoContainer.transform.GetChild(i).GetComponent<FeedData>())
+                            allPhotoContainer.transform.GetChild(i).GetComponent<FeedData>().SetFeedPrefab(currentPageAllTextPostWithUserIdRoot.data.rows[i], false);
+                        allPhotoContainer.transform.GetChild(i).name = "User Feed Post old one " + i;
+                    }
+                    catch
+                    {
+                        Debug.Log("Exception here");
+                    }
                     //allPhotoContainer.transform.GetChild(i).SetSiblingIndex(i);
                 }
                 else if (((!loadedMyPostAndVideoId.Contains(currentPageAllTextPostWithUserIdRoot.data.rows[i].id) && Feedparent == null) || (!loadedMyPostAndVideoIdInFeedPage.Contains(currentPageAllTextPostWithUserIdRoot.data.rows[i].id) && Feedparent != null))
