6574de257b88d56d5256dd44e6eee7e0bcab2def
WaqasAhmad-Unity <111582253+WaqasAhmad-Unity@users.noreply.github.com>
2024-03-21 15:25:46 +0500

    Xana Issue While going into sub worlds

--- Assets/Scripts/SceneManage.cs
+++ Assets/Scripts/SceneManage.cs
@@ -113,6 +113,12 @@ public class SceneManage : MonoBehaviourPunCallbacks
         XanaConstants.xanaConstants.isBuilderScene = false;
         XanaConstants.xanaConstants.JjWorldSceneChange = true;
         XanaConstants.xanaConstants.JjWorldTeleportSceneName = "XANA Lobby";
+
+        if ((ConstantsGod.API_BASEURL.Contains("test")))
+            XanaConstants.xanaConstants.MuseumID = "406";
+        else
+            XanaConstants.xanaConstants.MuseumID = "38";
+
         StartCoroutine(LoadMainEnumerator());
     }
     IEnumerator LoadMainEnumerator()
