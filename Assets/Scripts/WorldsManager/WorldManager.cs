89ae11413ede7f27b7a078f9d976fc67f75cf2b7
AhsanNaseerNBI <ahsannaseer@noborderz.com>
2024-04-23 12:15:20 +0500

    Update World Manager

--- Assets/Scripts/WorldsManager/WorldManager.cs
+++ Assets/Scripts/WorldsManager/WorldManager.cs
@@ -565,7 +565,6 @@ public class WorldManager : MonoBehaviour
                 if (!string.IsNullOrEmpty(_worldInfo.banner_new))
                 {
                     IThumbnailDownloadURL = _worldInfo.banner_new;
-
                     IThumbnailDownloadURL = _worldInfo.banner_new.Replace("https://cdn.xana.net/xanaprod", "https://aydvewoyxq.cloudimg.io/_xanaprod_/xanaprod");
                     // Test-net
                     IThumbnailDownloadURL = IThumbnailDownloadURL.Replace("https://cdn.xana.net/apitestxana/Defaults", "https://aydvewoyxq.cloudimg.io/_apitestxana_/apitestxana/Defaults");
@@ -586,6 +585,7 @@ public class WorldManager : MonoBehaviour
         }
         catch
         {
+            Debug.LogError("Check Exception world thumbnail Image");
             _event.ThumbnailDownloadURL = _worldInfo.thumbnail;
         }
         _event.BannerLink = _worldInfo.banner;
@@ -594,8 +594,7 @@ public class WorldManager : MonoBehaviour
         _event.PressedIndex = int.Parse(_worldInfo.id);
         _event.UpdatedAt = _worldInfo.updatedAt;
         _event.CreatedAt = _worldInfo.createdAt;
-        //_event.WorldVisitCount = _WorldInfo.data.rows[i].totalVisits; // TotalVisit Variable Used for Web
-        _event.WorldVisitCount = _worldInfo.xanaAppVisitCount;
+        _event.WorldVisitCount = _worldInfo.totalVisits;
         _event.isFavourite = _worldInfo.isFavourite;
         if (_worldInfo.tags != null)
             _event.WorldTags = _worldInfo.tags;
@@ -605,29 +604,31 @@ public class WorldManager : MonoBehaviour
             if (!string.IsNullOrEmpty(_worldInfo.user.userProfile.bio))
                 _event.CreatorDescription = _worldInfo.user.userProfile.bio;
 
-            _event.CreatorDescription = _worldInfo.user.userProfile.bio;
-        }
-        if (_worldInfo.user != null)
-        {
-            _event.UserAvatarURL = _worldInfo.user.avatar;
-        }
-        if (_worldInfo.entityType == WorldType.USER_WORLD.ToString())
-        {
-            _event.Creator_Name = _worldInfo.user.name;
-            _event.CreatorDescription = _worldInfo.creatorDetails.description;
-            _event.UserAvatarURL = _worldInfo.user.avatar;
-            _event.UserLimit = "15";
-        }
-        else
-        {
-            if (!string.IsNullOrEmpty(_worldInfo.user.name))
+            //_event.CreatorDescription = _WorldInfo.data.rows[i].user.userProfile.bio;
+
+            if (_worldInfo.entityType == WorldType.USER_WORLD.ToString())
+            {
                 _event.Creator_Name = _worldInfo.user.name;
+                //_event.CreatorDescription = _WorldInfo.data.rows[i].creatorDetails.description; // due to wrong API response commited this
+                _event.CreatorDescription = _worldInfo.user.userProfile.bio;
+                _event.UserAvatarURL = _worldInfo.user.avatar;
+                _event.UserLimit = "15";
+            }
             else
-                _event.Creator_Name = "XANA";
-            _event.UserLimit = _worldInfo.user_limit;
+            {
+                if (!string.IsNullOrEmpty(_worldInfo.user.name))
+                    _event.Creator_Name = _worldInfo.user.name;
+                else
+                    _event.Creator_Name = "XANA";
+
+                if (!string.IsNullOrEmpty(_worldInfo.creator))
+                    _event.Creator_Name = _worldInfo.creator;
+
+                if (!string.IsNullOrEmpty(_worldInfo.user.avatar))
+                    _event.UserAvatarURL = _worldInfo.user.avatar;
+            }
         }
-        if (!string.IsNullOrEmpty(_worldInfo.creator))
-            _event.Creator_Name = _worldInfo.creator;
+        _event.UserLimit = _worldInfo.user_limit;
 
         worldItemView.InitItem(_event);
     }
