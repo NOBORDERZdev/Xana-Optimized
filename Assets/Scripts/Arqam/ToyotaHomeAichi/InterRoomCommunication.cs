34e055b8c7c52091b324259ebde630c459010e09
Arqam Javed <122434224+ArqamNoBorderz@users.noreply.github.com>
2024-04-26 11:07:05 +0500

    singleton updated

--- Assets/Scripts/Arqam/ToyotaHomeAichi/InterRoomCommunication.cs
+++ Assets/Scripts/Arqam/ToyotaHomeAichi/InterRoomCommunication.cs
@@ -17,8 +17,8 @@ public class InterRoomCommunication : MonoBehaviour
         else
         {
             obj = this;
-        }
             DontDestroyOnLoad(this.gameObject);
+        }   
     }
 
     public void BroadcastUserJoinInterviewRoom(int userId)
