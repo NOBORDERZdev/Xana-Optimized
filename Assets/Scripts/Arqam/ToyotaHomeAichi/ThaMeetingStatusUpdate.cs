393f3b779967a3e7366f96e6161230e527a48cc9
ArqamNoBorderz <arqam.javed@noborderz.com>
2024-04-29 17:35:55 +0500

    update hashtable

--- Assets/Scripts/Arqam/ToyotaHomeAichi/ThaMeetingStatusUpdate.cs
+++ Assets/Scripts/Arqam/ToyotaHomeAichi/ThaMeetingStatusUpdate.cs
@@ -3,6 +3,7 @@ using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static ConstantsHolder;
 using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 public class ThaMeetingStatusUpdate : MonoBehaviour
@@ -12,16 +13,22 @@ public class ThaMeetingStatusUpdate : MonoBehaviour
     {
         Hashtable hash = new Hashtable();
         if (hash.ContainsKey("MeetingStatus")){
-            if (ArrowManager.Instance.GetComponent<PhotonView>().Controller.CustomProperties.TryGetValue("MeetingStatus", out object num))
-                ConstantsHolder.xanaConstants.toyotaMeetingStatus = (ConstantsHolder.MeetingStatus)num;
+            if (PhotonNetwork.LocalPlayer.CustomProperties.ContainsKey("MeetingStatus"))
+            {
+                ConstantsHolder.xanaConstants.toyotaMeetingStatus = (MeetingStatus)(int)PhotonNetwork.LocalPlayer.CustomProperties["MeetingStatus"];
+                Debug.LogError("Num:: " + (int)PhotonNetwork.LocalPlayer.CustomProperties["MeetingStatus"]);
+            }
         }
+        else
+            Debug.LogError("Meeting Status Not Exist");
     }
 
     public void UpdateMeetingStatus()
     {
         Hashtable hash = new Hashtable();
-        hash.Add("MeetingStatus", (int)ConstantsHolder.xanaConstants.toyotaMeetingStatus);
-        PhotonNetwork.LocalPlayer.SetCustomProperties(hash);
+        hash["MeetingStatus"] = (int)ConstantsHolder.xanaConstants.toyotaMeetingStatus;
+        PhotonNetwork.CurrentRoom.SetCustomProperties(hash);
+        Debug.LogError("Num:: " + (int)ConstantsHolder.xanaConstants.toyotaMeetingStatus);
     }
 
 
