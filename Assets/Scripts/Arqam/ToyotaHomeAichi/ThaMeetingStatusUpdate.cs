4291ba49095c29b46682a1daeb46ec52959e3aa0
KamranNBI <muhammad.kamran@noborderz.com>
2024-04-29 18:06:44 +0500

    Update Meeting Status Update script

--- Assets/Scripts/Arqam/ToyotaHomeAichi/ThaMeetingStatusUpdate.cs
+++ Assets/Scripts/Arqam/ToyotaHomeAichi/ThaMeetingStatusUpdate.cs
@@ -9,27 +9,61 @@ using Hashtable = ExitGames.Client.Photon.Hashtable;
 public class ThaMeetingStatusUpdate : MonoBehaviour
 {
     // Start is called before the first frame update
+    //void Start()
+    //{
+    //    Hashtable hash = new Hashtable();
+    //    if (hash.ContainsKey("MeetingStatus")){
+    //        if (PhotonNetwork.LocalPlayer.CustomProperties.ContainsKey("MeetingStatus"))
+    //        {
+    //            ConstantsHolder.xanaConstants.toyotaMeetingStatus = (MeetingStatus)(int)PhotonNetwork.LocalPlayer.CustomProperties["MeetingStatus"];
+    //            Debug.LogError("Num:: " + (int)PhotonNetwork.LocalPlayer.CustomProperties["MeetingStatus"]);
+    //        }
+    //    }
+    //    else
+    //        Debug.LogError("Meeting Status Not Exist");
+    //}
+
+    //public void UpdateMeetingStatus()
+    //{
+    //    Hashtable hash = new Hashtable();
+    //    hash["MeetingStatus"] = (int)ConstantsHolder.xanaConstants.toyotaMeetingStatus;
+    //    PhotonNetwork.CurrentRoom.SetCustomProperties(hash);
+    //    Debug.LogError("Num:: " + (int)ConstantsHolder.xanaConstants.toyotaMeetingStatus);
+    //}
+    // Start is called before the first frame update
     void Start()
     {
-        Hashtable hash = new Hashtable();
-        if (hash.ContainsKey("MeetingStatus")){
-            if (PhotonNetwork.LocalPlayer.CustomProperties.ContainsKey("MeetingStatus"))
+        if (PhotonNetwork.LocalPlayer.CustomProperties.ContainsKey("MeetingStatus"))
+        {
+            object status = PhotonNetwork.LocalPlayer.CustomProperties["MeetingStatus"];
+            if (status is int)
             {
-                ConstantsHolder.xanaConstants.toyotaMeetingStatus = (MeetingStatus)(int)PhotonNetwork.LocalPlayer.CustomProperties["MeetingStatus"];
-                Debug.LogError("Num:: " + (int)PhotonNetwork.LocalPlayer.CustomProperties["MeetingStatus"]);
+                ConstantsHolder.xanaConstants.toyotaMeetingStatus = (MeetingStatus)(int)status;
+                Debug.LogError("Meeting Status:: " + (int)status); 
             }
         }
         else
+        {
             Debug.LogError("Meeting Status Not Exist");
+        }
     }
 
     public void UpdateMeetingStatus()
     {
-        Hashtable hash = new Hashtable();
-        hash["MeetingStatus"] = (int)ConstantsHolder.xanaConstants.toyotaMeetingStatus;
-        PhotonNetwork.CurrentRoom.SetCustomProperties(hash);
-        Debug.LogError("Num:: " + (int)ConstantsHolder.xanaConstants.toyotaMeetingStatus);
-    }
+        Hashtable properties = new Hashtable
+        {
+            ["MeetingStatus"] = (int)ConstantsHolder.xanaConstants.toyotaMeetingStatus
+        };
 
+        if (PhotonNetwork.CurrentRoom != null)
+        {
+            PhotonNetwork.CurrentRoom.SetCustomProperties(properties);
+            Debug.LogError("Meeting Status Updated: " + (int)ConstantsHolder.xanaConstants.toyotaMeetingStatus);
+        }
+        else
+        {
+            Debug.LogError("Attempted to set custom properties on a null room.");
+        }
+    }
 
 }
