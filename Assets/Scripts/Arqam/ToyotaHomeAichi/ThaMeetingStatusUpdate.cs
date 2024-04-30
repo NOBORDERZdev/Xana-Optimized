e03d0c90f43d041a4b5084494932c5bdbedd6184
ArqamNoBorderz <arqam.javed@noborderz.com>
2024-04-30 13:58:01 +0500

    update photon custom property

--- Assets/Scripts/Arqam/ToyotaHomeAichi/ThaMeetingStatusUpdate.cs
+++ Assets/Scripts/Arqam/ToyotaHomeAichi/ThaMeetingStatusUpdate.cs
@@ -1,20 +1,49 @@
 using UnityEngine;
 using Photon.Pun;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+using Photon.Realtime;
 
-public class ThaMeetingStatusUpdate : MonoBehaviour
+public class ThaMeetingStatusUpdate : MonoBehaviourPunCallbacks
 {
     public enum MeetingStatus { End, Inprogress, HouseFull }
     public MeetingStatus tms;
 
+    private const string MeetingStatusPropertyName = "MeetingStatus";
 
     public void UpdateMeetingParams(int status)
     {
         //tms = (MeetingStatus)status;
-        this.GetComponent<PhotonView>().RPC(nameof(StartMeeting), RpcTarget.AllBuffered, status);
+        //this.GetComponent<PhotonView>().RPC(nameof(StartMeeting), RpcTarget.AllBuffered, status);
+        StartMeeting(status);
+
+        // Update the custom property for all players in the room
+        Hashtable hash = new Hashtable();
+        hash[MeetingStatusPropertyName] = status;
+        PhotonNetwork.CurrentRoom.SetCustomProperties(hash);
     }
 
+    public override void OnRoomPropertiesUpdate(Hashtable propertiesThatChanged)
+    {
+        base.OnRoomPropertiesUpdate(propertiesThatChanged);
 
-    [PunRPC]
+        // Check if the meeting status property was updated
+        if (propertiesThatChanged.ContainsKey(MeetingStatusPropertyName))
+        {
+            tms = (MeetingStatus)(int)propertiesThatChanged[MeetingStatusPropertyName];
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        base.OnPlayerEnteredRoom(newPlayer);
+        Debug.LogError("New Player join room");
+        // Send the current meeting status to the new player
+        Hashtable hash = new Hashtable();
+        hash[MeetingStatusPropertyName] = (int)tms;
+        newPlayer.SetCustomProperties(hash);
+    }
+
+    //[PunRPC]
     public void StartMeeting(int num) //, int ViewID
     {
         tms = (MeetingStatus)num;
