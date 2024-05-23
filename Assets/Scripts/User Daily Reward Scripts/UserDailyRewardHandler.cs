7df79e3f745b83027a901cfd5cd8b4f579900810
rajasohaib91 <134358961+SohaibTanveerNBI@users.noreply.github.com>
2024-05-23 17:23:42 +0500

    Daily reward changes
    Daily reward changes

--- Assets/Scripts/User Daily Reward Scripts/UserDailyRewardHandler.cs
+++ Assets/Scripts/User Daily Reward Scripts/UserDailyRewardHandler.cs
@@ -10,13 +10,13 @@ using TMPro;
 public class UserDailyRewardHandler : MonoBehaviour
 {
     [SerializeField]
-    private GameObject dailyRewardPopup;
+    private GameObject _dailyRewardPopup;
 
     [SerializeField]
-    private TextMeshProUGUI rewardedAmountText;
+    private TextMeshProUGUI _rewardedAmountText;
 
-    private SocketManager socketManager;
-    private int myUserId = 0;
+    private SocketManager _socketManager;
+    private int _myUserId = 0;
 
     private string SocketUrl
     {
@@ -37,28 +37,28 @@ public class UserDailyRewardHandler : MonoBehaviour
     {
         if (SocketUrl != null)
         {
-            socketManager = new SocketManager(new Uri(SocketUrl));
-            socketManager.Socket.On<ConnectResponse>(SocketIOEventTypes.Connect, OnSocketConnected);
-            socketManager.Socket.On<CustomError>(SocketIOEventTypes.Error, OnSocketError);
+            _socketManager = new SocketManager(new Uri(SocketUrl));
+            _socketManager.Socket.On<ConnectResponse>(SocketIOEventTypes.Connect, OnSocketConnected);
+            _socketManager.Socket.On<CustomError>(SocketIOEventTypes.Error, OnSocketError);
         }
         DontDestroyOnLoad(this);
 
         while (ConstantsHolder.userId == null)
             yield return new WaitForSeconds(0.5f);
 
-        myUserId = int.Parse(ConstantsHolder.userId);
+        _myUserId = int.Parse(ConstantsHolder.userId);
     }
     private void OnDisable()
     {
-        if (socketManager != null)
+        if (_socketManager != null)
         {
-            socketManager.Socket.Off();
-            socketManager.Close();
+            _socketManager.Socket.Off();
+            _socketManager.Close();
         }
     }
     private void OnSocketConnected(ConnectResponse resp)
     {
-        socketManager.Socket.On<string>("xeny-rewarded", DailyRewardResponse);
+        _socketManager.Socket.On<string>("xeny-rewarded", DailyRewardResponse);
     }
 
     private void OnSocketError(CustomError args)
@@ -69,9 +69,9 @@ public class UserDailyRewardHandler : MonoBehaviour
     private void DailyRewardResponse(string resp)
     {
         UserDailyRewardData data = JsonConvert.DeserializeObject<UserDailyRewardData>(resp);
-        if (data.userId == myUserId)
+        if (data.userId == _myUserId)
         {
-            rewardedAmountText.text = data.amount.ToString();   
+            _rewardedAmountText.text = data.amount.ToString();   
             StartCoroutine(ShowDailyRewardRoutine());
         }
     }
@@ -81,7 +81,7 @@ public class UserDailyRewardHandler : MonoBehaviour
         while (SceneManager.GetActiveScene().name != "Home")
             yield return new WaitForSecondsRealtime(5f);
 
-        dailyRewardPopup.SetActive(true);
+        _dailyRewardPopup.SetActive(true);
         StopCoroutine(ShowDailyRewardRoutine());
     }
 }
