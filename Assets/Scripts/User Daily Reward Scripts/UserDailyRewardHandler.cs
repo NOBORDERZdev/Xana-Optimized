de23a0299f12a3fea8f453d7c47b1096c023bbec
rajasohaib91 <134358961+SohaibTanveerNBI@users.noreply.github.com>
2024-05-24 11:50:59 +0500

    Added daily reward functionality to home scene
    Added daily reward functionality to home scene

---
+++ Assets/Scripts/User Daily Reward Scripts/UserDailyRewardHandler.cs
@@ -0,0 +1,149 @@
+using System;
+using UnityEngine;
+using BestHTTP.SocketIO3;
+using BestHTTP.SocketIO3.Events;
+using Newtonsoft.Json;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using TMPro;
+using ExitGames.Client.Photon;
+
+public class UserDailyRewardHandler : MonoBehaviour
+{
+    private static UserDailyRewardHandler _instance;
+
+    [SerializeField]
+    private GameObject _dailyRewardPopup;
+
+    [SerializeField]
+    private TextMeshProUGUI _rewardedAmountText;
+
+    private SocketManager _socketManager;
+    private int _myUserId = 0;
+    private bool _hasToShowDailyPopup = false;
+
+    private string SocketUrl
+    {
+        get
+        {
+            if (APIBasepointManager.instance.IsXanaLive)
+            {
+                return "LiveURL";
+            }
+            else
+            {
+                return "https://mslog-test.xana.net";
+            }
+        }
+    }
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+    }
+    private IEnumerator Start()
+    {
+
+        if (SocketUrl != null)
+        {
+            _socketManager = new SocketManager(new Uri(SocketUrl));
+            _socketManager.Socket.On<ConnectResponse>(SocketIOEventTypes.Connect, OnSocketConnected);
+            _socketManager.Socket.On<CustomError>(SocketIOEventTypes.Error, OnSocketError);
+            _socketManager.Socket.On<CustomError>(SocketIOEventTypes.Disconnect, OnSocketDisconnect);
+        }
+
+        while (ConstantsHolder.userId == null)
+            yield return new WaitForSeconds(0.5f);
+
+        _myUserId = int.Parse(ConstantsHolder.userId);
+    }
+
+    private void OnDisable()
+    {
+        if (_socketManager != null)
+        {
+            _socketManager.Socket.Off();
+            _socketManager.Close();
+        }
+
+    }
+
+    private void OnSocketConnected(ConnectResponse resp)
+    {
+        Debug.Log("Sohaib Socket Connected : " + resp);
+        _socketManager.Socket.On<string>("xeny-rewarded", DailyRewardResponse);
+    }
+
+    private void OnSocketError(CustomError args)
+    {
+        Debug.LogError("Sohaib Socket Error : " + args);
+    }
+
+    private void OnSocketDisconnect(CustomError args)
+    {
+        Debug.LogError("Sohaib Socket Disconnected : " + args);
+    }
+    private void DailyRewardResponse(string resp)
+    {
+        UserDailyRewardData data = JsonConvert.DeserializeObject<UserDailyRewardData>(resp);
+
+        if (data.userId == _myUserId)
+        {
+            _rewardedAmountText.text = data.amount.ToString();
+            if (SceneManager.GetActiveScene().name == "Home")
+            {
+                ShowDailyRewardPopup();
+            }
+            else
+            {
+                _hasToShowDailyPopup = true;
+            }
+
+        }
+    }
+    private void ShowDailyRewardPopup()
+    {
+        _dailyRewardPopup.SetActive(true);
+        _hasToShowDailyPopup = false;
+    }
+
+    //Executes when Home Scene is loaded
+    private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
+    {
+        if (arg0.name == "Home" && _hasToShowDailyPopup)
+        {
+            Debug.Log("Home Scene Loaded");
+            StartCoroutine(ShowDailyRewardRoutine());
+        }
+    }
+
+    private IEnumerator ShowDailyRewardRoutine()
+    {
+        //Adding delay to avoid showing the popup on the lobby worlds loading throuth Home scene
+        yield return new WaitForSecondsRealtime(3f);
+        if (SceneManager.GetActiveScene().name == "Home")
+        {
+            ShowDailyRewardPopup();
+        }
+        StopCoroutine(ShowDailyRewardRoutine());
+    }
+
+}
+
+[Serializable]
+public struct UserDailyRewardData
+{
+    public int userId;
+    public int amount;
+    public DateTime datetime;
+}
