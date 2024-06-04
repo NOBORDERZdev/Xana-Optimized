3b4e80334eebe28deb98504664ac174464aba991
Hassan Sohail <107843990+HassanSohail000@users.noreply.github.com>
2024-06-04 16:38:30 +0500

    Update MutiplayerController.cs

--- Assets/Scripts/MutiplayerController.cs
+++ Assets/Scripts/MutiplayerController.cs
@@ -344,8 +344,8 @@ namespace Photon.Pun.Demo.PunBasics
 
         public override void OnMasterClientSwitched(Player newMasterClient)
         {
-            if (ConstantsHolder.xanaConstants.isBuilderScene)
-                GamificationComponentData.instance.MasterClientSwitched(newMasterClient);
+            //if (ConstantsHolder.xanaConstants.isBuilderScene)
+            //    GamificationComponentData.instance.MasterClientSwitched(newMasterClient);
         }
         #endregion
 
