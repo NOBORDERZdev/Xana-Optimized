7e7ca9f686ccc1a2025b9676a225e760254e582e
Hassan Sohail <107843990+HassanSohail000@users.noreply.github.com>
2024-06-04 12:53:09 +0500

    update

--- Assets/Scripts/XANA Party/XANAPartyManager.cs
+++ Assets/Scripts/XANA Party/XANAPartyManager.cs
@@ -140,7 +140,6 @@ public class XANAPartyManager : MonoBehaviour
 
         HideLoadingScreens();
         ConstantsHolder.xanaConstants.EnviornmentName = ConstantsHolder.xanaConstants.XanaPartyGameName;
-        ConstantsHolder.xanaConstants.isBuilderScene = true;
         WorldItemView.m_EnvName = ConstantsHolder.xanaConstants.XanaPartyGameName;
         WorldManager.instance.PlayWorld();
     }
