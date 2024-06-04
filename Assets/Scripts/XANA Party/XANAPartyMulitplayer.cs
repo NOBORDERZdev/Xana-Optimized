03f93bd794cff2b288f07e1fa98fb502c0697e3b
Hassan Sohail <107843990+HassanSohail000@users.noreply.github.com>
2024-06-04 15:57:01 +0500

    master creating room

--- Assets/Scripts/XANA Party/XANAPartyMulitplayer.cs
+++ Assets/Scripts/XANA Party/XANAPartyMulitplayer.cs
@@ -1,11 +1,14 @@
 using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.XR;
 using UnityEngine;
 
 public class XANAPartyMulitplayer : MonoBehaviour
 {
     bool singleTimeCall = true;
+    private RoomOptions roomOptions;
 
     public IEnumerator MovePlayersToRandomGame()
     {
@@ -15,11 +18,34 @@ public class XANAPartyMulitplayer : MonoBehaviour
         // Select a random room
         //string newRoom = GetComponent<MultiplayerXanaParty>().GetXanaPartyWorld();
         print("~~~~~~ calling gameID" + XANAPartyManager.Instance.name);
-        GameData gameId =XANAPartyManager.Instance.GetRandomAndRemove();
-        print("GAME ID "+ gameId.Id + " : "+ gameId.WorldName);
+        GameData gameId = XANAPartyManager.Instance.GetRandomAndRemove();
+        print("GAME ID " + gameId.Id + " : " + gameId.WorldName);
+
         yield return new WaitForSeconds(1f);
-        GameplayEntityLoader.instance.PenguinPlayer.GetComponent<PhotonView>().RPC(nameof(MovePlayersToRoom), RpcTarget.AllBuffered, gameId.Id, gameId.WorldName); // Calling RPC from Master
+        GameplayEntityLoader.instance.PenguinPlayer.GetComponent<PhotonView>().RPC(nameof(MovePlayersToRoom), RpcTarget.OthersBuffered, gameId.Id, gameId.WorldName); // Calling RPC from Master
+        string roomName;
+        //do
+        //{
+        //    roomName = PhotonNetwork.CurrentLobby.Name + UnityEngine.Random.Range(0, 9999).ToString();
+        //}
+        //while (roomNames.Contains(roomName));
+        roomName = gameId.WorldName + UnityEngine.Random.Range(0, 9999).ToString();;
+
+        PhotonNetwork.JoinOrCreateRoom(roomName, RoomOptionsRequest(), new TypedLobby(roomName, LobbyType.Default));
+
     }
+
+     public RoomOptions RoomOptionsRequest()
+    {
+            roomOptions = new RoomOptions();
+            roomOptions.MaxPlayers = (byte)(int.Parse(ConstantsHolder.xanaConstants.userLimit));
+            roomOptions.IsOpen = true;
+            roomOptions.IsVisible = true;
+
+            roomOptions.PublishUserId = true;
+            roomOptions.CleanupCacheOnLeave = true;
+            return roomOptions;
+        }
 
     [PunRPC]
     public void MovePlayersToRoom(int gameId, string gameName)
