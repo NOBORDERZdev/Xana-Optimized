abb7eca9c4a758c1e76ce4187493e19b779649ab
Hassan Sohail <107843990+HassanSohail000@users.noreply.github.com>
2024-06-07 19:51:25 +0500

    Update GamificationComponentData.cs

--- Assets/Scripts/Builders/GameCreatorLogic/GamificationComponentData.cs
+++ Assets/Scripts/Builders/GameCreatorLogic/GamificationComponentData.cs
@@ -432,14 +432,14 @@ public class GamificationComponentData : MonoBehaviourPunCallbacks
             allPalyerReady = true;
         }
         print("invoke action of counter");
-        new Delayed.Action(() => { BuilderEventManager.XANAPartyRaceStart?.Invoke(); }, 5f);
-        //GetComponent<PhotonView>().RPC(nameof(StartGameRPC), RpcTarget.All);
+        //new Delayed.Action(() => { BuilderEventManager.XANAPartyRaceStart?.Invoke(); }, 5f);
+        GetComponent<PhotonView>().RPC(nameof(StartGameRPC), RpcTarget.All);
     }
-    //[PunRPC]
-    //void StartGameRPC()
-    //{
-    //    new Delayed.Action(() => { BuilderEventManager.XANAPartyRaceStart?.Invoke(); }, 5f);
-    //}
+    [PunRPC]
+    void StartGameRPC()
+    {
+        new Delayed.Action(() => { BuilderEventManager.XANAPartyRaceStart?.Invoke(); }, 5f);
+    }
     #endregion
 }
 
