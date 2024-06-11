c513f87739f979ddcf4ddf18742ffc99d93e18da
KamranNBI <muhammad.kamran@noborderz.com>
2024-06-11 18:27:42 +0500

    Delete Account popup UI issues according to figma fixed

--- Assets/Scripts/AccountDeleteToggle.cs
+++ Assets/Scripts/AccountDeleteToggle.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class AccountDeleteToggle : MonoBehaviour
 {
-    public Toggle _toggle;
-    public Button _button;
+    public Toggle Toggle;
+    public Button Button;
+    public TextMeshProUGUI DescriptionText;
    public void ChangeButtonOnToggle()
    {
-        if (_toggle.isOn)
-            _button.interactable = true;
+        if (Toggle.isOn)
+        {
+            Button.interactable = true;
+            DescriptionText.color = new Color32(0, 0, 0, 255);
+        }
         else
-            _button.interactable = false;
+        {
+            Button.interactable = false;
+            DescriptionText.color = new Color32(65, 65, 65, 100);
+        }
 
    }
 }
