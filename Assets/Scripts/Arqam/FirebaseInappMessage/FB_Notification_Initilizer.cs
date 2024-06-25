8ead143b94c847b0c231419ebeb2aaac885f0769
KamranNBI <muhammad.kamran@noborderz.com>
2024-06-25 17:07:45 +0500

    Update Script

--- Assets/Scripts/Arqam/FirebaseInappMessage/FB_Notification_Initilizer.cs
+++ Assets/Scripts/Arqam/FirebaseInappMessage/FB_Notification_Initilizer.cs
@@ -180,7 +180,6 @@ public class FB_Notification_Initilizer : MonoBehaviour
 
     public virtual void OnTokenReceived(object sender, TokenReceivedEventArgs token)
     {
-        Handheld.Vibrate();
         onReceiveToken?.Invoke(token.Token);
         Debug.Log("Token Generated: " + token.Token);
     }
