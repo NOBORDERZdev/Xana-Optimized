b4f26ded9b7e676af7f53796c0cec3ebaddbe190
Ali Hamza <alihamzaunity@noborderz.com>
2024-06-28 21:34:47 +0500

    Counter Update same as Active Player in level

--- Assets/Scripts/Usman Scripts/ReferencesForGamePlay.cs
+++ Assets/Scripts/Usman Scripts/ReferencesForGamePlay.cs
@@ -126,7 +126,16 @@ public class ReferencesForGamePlay : MonoBehaviour
         {
             RoomMaxPlayerCount = Convert.ToInt32(ConstantsHolder.xanaConstants.userLimit);
             if (PhotonNetwork.CurrentRoom != null)
-                PlayerCount = Convert.ToInt32(PhotonNetwork.CurrentRoom.PlayerCount);
+            {
+                if (ConstantsHolder.xanaConstants.isXanaPartyWorld && ConstantsHolder.xanaConstants.isJoinigXanaPartyGame)
+                {
+                    PlayerCount = XANAPartyManager.Instance.ActivePlayerInCurrentLevel;
+                }
+                else
+                {
+                    PlayerCount = Convert.ToInt32(PhotonNetwork.CurrentRoom.PlayerCount);
+                }
+            }
         }
         if (instance != null && instance != this/* && !FeedEventPrefab.m_EnvName.Contains("XANA Lobby")*/)
         {
@@ -336,7 +345,14 @@ public class ReferencesForGamePlay : MonoBehaviour
                     }
                     else
                     {
-                        PlayerCount = Convert.ToInt32(PhotonNetwork.CurrentRoom.PlayerCount);
+                        if (ConstantsHolder.xanaConstants.isXanaPartyWorld && ConstantsHolder.xanaConstants.isJoinigXanaPartyGame)
+                        {
+                            PlayerCount = XANAPartyManager.Instance.ActivePlayerInCurrentLevel;
+                        }
+                        else
+                        {
+                            PlayerCount = Convert.ToInt32(PhotonNetwork.CurrentRoom.PlayerCount);
+                        }
                     }
                     totalCounter.text = PlayerCount + "/" + RoomMaxPlayerCount /*ConstantsHolder.xanaConstants.userLimit*/;
 
