c3dd1aef2c8f12ea16f9ba575a77cbebc3ed7cd9
Abdullah Rashid <abdullah.rashid@noborderz.com>
2024-07-05 13:41:21 +0500

    Update Frame Rate as per Performance and heat issue

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -145,7 +145,7 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         Screen.sleepTimeout = SleepTimeout.NeverSleep;
-        Application.targetFrameRate = 60;
+        Application.targetFrameRate = 30;
         OnceGuestBool = false;
         OnceLoginBool = false;
     }
