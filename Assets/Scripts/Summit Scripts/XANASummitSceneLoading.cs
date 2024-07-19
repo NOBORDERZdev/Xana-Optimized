a2e24440a4eb5b60d297af365600f1b49c12a913
Amit Patidar <amit.patidar@noborderz.com>
2024-07-19 17:35:26 +0530

    on back to summit fix.

--- Assets/Scripts/Summit Scripts/XANASummitSceneLoading.cs
+++ Assets/Scripts/Summit Scripts/XANASummitSceneLoading.cs
@@ -78,9 +78,9 @@ public class XANASummitSceneLoading : MonoBehaviour
 
         GetPlayerPosition(playerPos);
         string existingSceneName = WorldItemView.m_EnvName;
+        ConstantsHolder.loadedScenes.Push(existingSceneName);
         WorldItemView.m_EnvName = domeGeneralData.world;
         ConstantsHolder.xanaConstants.EnviornmentName = domeGeneralData.world;
-        ConstantsHolder.loadedScenes.Push(ConstantsHolder.xanaConstants.EnviornmentName);
         previousUserLimit = ConstantsHolder.userLimit;
         ConstantsHolder.userLimit = domeGeneralData.maxPlayer;
         ConstantsHolder.isPenguin = domeGeneralData.IsPenguin;
@@ -116,7 +116,7 @@ public class XANASummitSceneLoading : MonoBehaviour
         SummitMiniMapStatusOnSceneChange(false);
         GetPlayerPosition(playerPos);
         string existingSceneName = WorldItemView.m_EnvName;
-        ConstantsHolder.loadedScenes.Push(ConstantsHolder.xanaConstants.EnviornmentName);
+        ConstantsHolder.loadedScenes.Push(existingSceneName);
         WorldItemView.m_EnvName = SceneName;
         ConstantsHolder.xanaConstants.EnviornmentName = SceneName;
         previousUserLimit = ConstantsHolder.userLimit;
