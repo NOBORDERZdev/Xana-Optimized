c9aaed235fdf6150a66e7533a7374d7a52efa914
Abdullah Rashid <abdullah.rashid@noborderz.com>
2024-07-23 15:57:27 +0500

    Updated UI and script For Guest Login

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -175,7 +175,7 @@ public class GameManager : MonoBehaviour
     }
     public void NotNowOfSignManager()
     {
-        UserLoginSignupManager.instance.GetComponent<OnEnableDisable>().ClosePopUp();
+        UserLoginSignupManager.instance.LoginRegisterScreen.SetActive(false);
        
         if (UiManager.HomePage.activeInHierarchy )
             UiManager.HomePage.SetActive(false);
@@ -199,7 +199,7 @@ public class GameManager : MonoBehaviour
         UiManager.AvaterButtonCustomPushed();
         AvatarCustomizationUIHandler.Instance.LoadMyClothCustomizationPanel();
         //Debug.Log("IsLoggedIn VALUEeeeeeeeee" + (PlayerPrefs.GetInt("IsLoggedIn")));
-        if (ConstantsHolder.loggedIn) 
+        if (ConstantsHolder.loggedIn || UserLoginSignupManager.instance.LoggedInAsGuest) 
         {
             UiManager.HomePage.SetActive(false);
             InventoryManager.instance.SignUpAndLoginPanel(3);
