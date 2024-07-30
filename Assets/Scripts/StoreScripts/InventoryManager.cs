805caaf95ffd8db0bc2b987392ea45fd57d339dc
WaqasAhmad-Unity <111582253+WaqasAhmad-Unity@users.noreply.github.com>
2024-07-30 12:27:18 +0500

    Fixing
    -Store Assets Tag Fixing
    -Image Kit -- URL Update
    -Null Exception -- when Return From Gameplay

--- Assets/Scripts/StoreScripts/InventoryManager.cs
+++ Assets/Scripts/StoreScripts/InventoryManager.cs
@@ -3281,6 +3281,8 @@ public class InventoryManager : MonoBehaviour
                         TempEnumVar = EnumClass.CategoryEnum.EyeBrowAvatar;
                         eyeBrowTapButton.SetActive(true);
 
+                        break; // currently there is no eyebrow available
+
                         for (int i = 1; i < AllCategoriesData[20].parentObj.transform.childCount; i++)
                         {
                             if (AllCategoriesData[20].parentObj.transform.GetChild(i).GetComponent<ItemDetail>().id.ParseToInt() == SaveCharacterProperties.instance.characterController.eyeBrowId)
@@ -3429,7 +3431,7 @@ public class InventoryManager : MonoBehaviour
 
         // Implement Image Kit URL
         string ImageKitUrl = dataListOfItems[objId].iconLink;
-        ImageKitUrl = ImageKitUrl.Replace("https://cdn.xana.net/xanaprod/Defaults/", "https://ik.imagekit.io/xanalia/xanaprod/Defaults/"); 
+        ImageKitUrl = ImageKitUrl.Replace("https://cdn.xana.net/xanaprod/Defaults/", "https://aydvewoyxq.cloudimg.io/_xanaprod_/xanaprod/Defaults/"); 
         ImageKitUrl+= "?width=128&height=128";
 
 
