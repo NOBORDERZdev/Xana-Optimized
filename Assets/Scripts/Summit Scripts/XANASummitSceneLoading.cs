1c2902d5712200b3fe578cf7e7d824b708a3e4a5
AhsanNaseerNBI <ahsannaseer@noborderz.com>
2024-08-07 13:44:57 +0500

    Fixes

--- Assets/Scripts/Summit Scripts/XANASummitSceneLoading.cs
+++ Assets/Scripts/Summit Scripts/XANASummitSceneLoading.cs
@@ -153,6 +153,7 @@ public class XANASummitSceneLoading : MonoBehaviour
                 _stayTimeTrackerForSummit.DomeWorldId = domeGeneralData.builderWorldId;
             else
                 _stayTimeTrackerForSummit.DomeWorldId = domeGeneralData.worldId;
+            _stayTimeTrackerForSummit.isBuilderWorld = domeGeneralData.worldType;
             _stayTimeTrackerForSummit.StartTrackingTime();
         }
         string eventName;
