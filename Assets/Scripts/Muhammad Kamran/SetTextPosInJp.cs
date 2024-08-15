68f221b0830c765d4e5a3f8047a1888fe9bb47ba
KamranNBI <muhammad.kamran@noborderz.com>
2024-08-15 11:41:44 +0500

    Localization and BG icon issues resolved

--- Assets/Scripts/Muhammad Kamran/SetTextPosInJp.cs
+++ Assets/Scripts/Muhammad Kamran/SetTextPosInJp.cs
@@ -6,12 +6,16 @@ public class SetTextPosInJp : MonoBehaviour
 {
     public RectTransform SignUpText1;
     public RectTransform SignUpText2;
+    public RectTransform PrivacyText1;
+    public RectTransform PrivacyText2;
     private void Start()
     {
         if (CustomLocalization.forceJapanese || GameManager.currentLanguage == "ja")
         {
             SignUpText1.localPosition = new Vector2(41, 12);
             SignUpText2.localPosition = new Vector2(315, 12);
+            PrivacyText1.localPosition = new Vector2(74, 0);
+            PrivacyText2.localPosition = new Vector2(74, 0);
         }
     }
 }
