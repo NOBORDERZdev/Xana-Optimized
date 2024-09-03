c7f512143b610db29442d1f33c883889d9c7f2f2
ArqamNoBorderz <arqam.javed@noborderz.com>
2024-09-03 19:26:01 +0500

    update file picker plugin

--- Assets/Scripts/Arqam/PMY/PDFPicker.cs
+++ Assets/Scripts/Arqam/PMY/PDFPicker.cs
@@ -1,29 +1,21 @@
 using UnityEngine;
-using System.IO;
 using Paroxe.PdfRenderer;
-using SimpleFileBrowser;
+
 public class PDFPicker : MonoBehaviour
 {
     public PDFViewer pdfViewer; // Assign your PDFRenderer component here
     public string filePath;
     public void PickPDF()
     {
-        FileBrowser.SetFilters(true, new FileBrowser.Filter("PDF Files", ".pdf"));
-        FileBrowser.ShowLoadDialog((paths) => { LoadAndRenderPDF(paths[0]); }, null, FileBrowser.PickMode.Files, false, "Select PDF", "Select");
+        NativeFilePicker.Permission permission = NativeFilePicker.PickFile((path) =>
+        {
+            if (path != null)
+            {
+                pdfViewer.FilePath = path;
+                pdfViewer.transform.root.gameObject.SetActive(true);
+                // Load your file here
+            }
+        }, new string[] { "application/pdf" });
     }
 
-    private void LoadAndRenderPDF(string path)
-    {
-        if (pdfViewer != null)
-        {
-            // byte[] pdfData = File.ReadAllBytes(path);
-            // pdfRenderer.LoadPDF(pdfData); // Implement PDF loading in your renderer
-            pdfViewer.FilePath = path;
-            pdfViewer.transform.root.gameObject.SetActive(true);
-        }
-        else
-        {
-            Debug.LogError("PDF Renderer is not assigned.");
-        }
-    }
 }
