797c73cb46ec55cee9e3366d579c98570e5baa5a
Pak-Mac <devbuild@noborderz.com>
2024-09-05 12:30:04 +0500

    resolve ios pdf issue

--- Assets/Scripts/Arqam/PMY/PDFPicker.cs
+++ Assets/Scripts/Arqam/PMY/PDFPicker.cs
@@ -1,9 +1,6 @@
 using UnityEngine;
 using Paroxe.PdfRenderer;
-//using UnityEngine.Android;
-//#if UNITY_IOS
-//using UnityEngine.iOS;
-//#endif
+
 
 public class PDFPicker : MonoBehaviour
 {
@@ -23,37 +20,21 @@ public class PDFPicker : MonoBehaviour
         }, new string[] { "application/pdf" });
 
 #elif UNITY_IOS
-string[] fileTypes = new string[] { "public.pdf" };
 
-NativeFilePicker.PickFile((string path) =>
-{
-    if (path == null)
-    {
-        Debug.LogError("File picking was cancelled or failed on iOS.");
-        return;
-    }
+        NativeFilePicker.PickFile((string path) =>
+        {
+            if (path == null)
+            {
+                Debug.LogError("File picking was cancelled or failed on iOS.");
+                return;
+            }
 
-    Debug.Log("Picked PDF file path: " + path);
+            Debug.Log("Picked PDF file path: " + path);
 
-    // Do something with the PDF file, e.g., open it
-     OpenDocumentPicker(path);
+            // Attempt to open the file using the iOS file system
+            OpenDocumentPicker(path);
 
-}, fileTypes);
-
-//NativeFilePicker.PickFile((string path) =>
-//{
-//    if (path == null)
-//    {
-//        Debug.LogError("File picking was cancelled or failed on iOS.");
-//        return;
-//    }
-
-//    Debug.Log("Picked PDF file path: " + path);
-
-//    // Attempt to open the file using the iOS file system
-//    OpenDocumentPicker(path);
-
-//}, new string[] { "public.pdf" });
+        }, new string[] { "com.adobe.pdf" });
 
 #endif
     }
