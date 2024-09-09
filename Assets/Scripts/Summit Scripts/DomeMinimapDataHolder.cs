3c6df50db22107a56fec60d4b33fcd690941ad60
gautam <gautam.rojasara@noborderz.com>
2024-09-09 19:27:56 +0530

    Added check for don't go through any place wia minimap when Gient wheel join

--- Assets/Scripts/Summit Scripts/DomeMinimapDataHolder.cs
+++ Assets/Scripts/Summit Scripts/DomeMinimapDataHolder.cs
@@ -132,6 +132,9 @@ public class DomeMinimapDataHolder : MonoBehaviour
     }
     public void OnClickYesBtn()
     {
+        if (GiantWheelManager.Instance != null && GiantWheelManager.Instance.CarAdded)
+            return;
+
         ConfirmationPanelHandling(false);
         ReferencesForGamePlay.instance.FullScreenMapStatus(false);
 
