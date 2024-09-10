ddb258d6925177321a98d668191ae049cbe8d0d4
SuyashNoborderz <118811897+SuyashNoborderz@users.noreply.github.com>
2024-09-10 19:11:39 +0530

    Fixed Logout and Delete account feature

--- Assets/LogoutManager.cs
+++ Assets/LogoutManager.cs
@@ -2,9 +2,25 @@ using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LogoutManager : MonoBehaviour
 {
+    public GameObject logoutPanel;
+    public GameObject profileButton;
+
+    private void Start()
+    {
+        if(SceneManager.GetSceneByName("Builder").isLoaded)
+        {
+            profileButton.SetActive(false);
+        }
+        else
+        {
+            profileButton.SetActive(true);
+        }
+    }
+
     public void OnClickLogoutButton()
     {
         UserLoginSignupManager.instance.LogoutAccount();
@@ -12,10 +28,20 @@ public class LogoutManager : MonoBehaviour
         if (PlayerPrefs.GetInt("ShowLiveUserCounter") == 1)
         {
             Debug.Log("LogoutDone");
+            logoutPanel.SetActive(false);
         }
         PlayerPrefs.SetInt("shownWelcome", 0);
         PlayerPrefs.SetString("UserNameAndPassword", "");
         GameManager.Instance.SpaceWorldManagerRef.worldSpaceHomeScreenRef.OnLogoutClearSpaceData();
         GlobalVeriableClass.callingScreen = "";
+    }
+
+    public void DeleteAccount()
+    {
+        UserLoginSignupManager.instance.DeleteAccount(() =>
+        {
+            Debug.Log("Account Deleted");
+            PhotonNetwork.Disconnect();
+        });
     }
 }
