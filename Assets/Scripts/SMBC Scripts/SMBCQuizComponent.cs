be09acfe559e6568a84533e53d13e6294dd65372
gautam <gautam.rojasara@noborderz.com>
2024-09-11 18:31:07 +0530

    Update SMBCQuizComponent.cs

--- Assets/Scripts/SMBC Scripts/SMBCQuizComponent.cs
+++ Assets/Scripts/SMBC Scripts/SMBCQuizComponent.cs
@@ -28,7 +28,7 @@ public class SMBCQuizComponent : MonoBehaviour
     {
         if (other.gameObject.tag == "PhotonLocalPlayer" && other.gameObject.GetComponent<PhotonView>().IsMine)
         {
-            if (!SMBCManager.Instance.CheckForObjectCollectible(RequireCollectible))
+            if (SMBCManager.Instance.CheckForObjectCollectible(RequireCollectible))
                 BuilderEventManager.OnSMBCQuizComponentCollisionEnter?.Invoke(this, _quizComponentData);
             else
                 BuilderEventManager.OnDoorKeyCollisionEnter?.Invoke("Please collect all require keys first!!");
