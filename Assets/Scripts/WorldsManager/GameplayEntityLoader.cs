0d41dcea2894ab00cc33f73b096125d40089a9ba
gautam <gautam.rojasara@noborderz.com>
2024-09-17 14:06:38 +0530

    Dome thumbnail image open issue | Selfie issue after fall down

--- Assets/Scripts/WorldsManager/GameplayEntityLoader.cs
+++ Assets/Scripts/WorldsManager/GameplayEntityLoader.cs
@@ -1022,6 +1022,9 @@ public class GameplayEntityLoader : MonoBehaviourPunCallbacks, IPunInstantiateMa
 
     void ResetPlayerPosition()
     {
+        //Stop selfi functionality when respawn after fall down
+        PlayerSelfieController.Instance.DisableSelfieFeature();
+
         if (ConstantsHolder.xanaConstants.isBuilderScene)
         {
             //Player respawn at spawn point after jump down from world
