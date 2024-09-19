bb9951ae09c41d09e80fa8c6d4e755a36b254f2f
Hassan Sohail <107843990+HassanSohail000@users.noreply.github.com>
2024-09-19 13:43:54 +0500

    Update ConstantsHolder.cs

--- Assets/Scripts/Gameplay/ConstantsHolder.cs
+++ Assets/Scripts/Gameplay/ConstantsHolder.cs
@@ -104,7 +104,6 @@ public class ConstantsHolder : MonoBehaviour
     public bool isBackFromWorld = false;
     public String MuseumID;
 
-    public bool isXanaPartyWorld = false;
     //For Metabuzz Environments
     public enum ComingFrom
     {
