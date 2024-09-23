a1654b9cea0f8c320693d052060f6f38aa404272
SuyashNoborderz <118811897+SuyashNoborderz@users.noreply.github.com>
2024-09-23 19:47:43 +0530

    Bug Fixes
    1. Camera issue Fixes in penguin.
    2. Penguin character respawn issue Fixed

--- Assets/Scripts/WorldsManager/GameplayEntityLoader.cs
+++ Assets/Scripts/WorldsManager/GameplayEntityLoader.cs
@@ -246,7 +246,8 @@ public class GameplayEntityLoader : MonoBehaviourPunCallbacks, IPunInstantiateMa
         if (currentEnvironment == null)
         {
             if (ConstantsHolder.xanaConstants.isBuilderScene)
-                SetupEnvirnmentForBuidlerScene();
+                StartCoroutine(WaitForMapDownload());
+
             else
             {
                 LoadEnvironment(ConstantsHolder.xanaConstants.EnviornmentName);
@@ -763,6 +764,7 @@ public class GameplayEntityLoader : MonoBehaviourPunCallbacks, IPunInstantiateMa
             DashButton.SetActive(false);
             XanaWorldController.SetActive(false);
             XanaPartyController.SetActive(true);
+            Debug.LogError("Xana Player Penguin.......");
             player = PhotonNetwork.Instantiate("XanaPenguin", spawnPoint, Quaternion.identity, 0);
             PenguinPlayer = player;
             mainController = player;
@@ -1174,6 +1176,18 @@ public class GameplayEntityLoader : MonoBehaviourPunCallbacks, IPunInstantiateMa
             LoadingHandler.CompleteSlider?.Invoke();
         }
         StartCoroutine(DownloadAssets());
+    }
+
+    IEnumerator WaitForMapDownload()
+    {
+        if (ConstantsHolder.xanaConstants.isXanaPartyWorld)
+        {
+            while (!BuilderAssetDownloader.isSpawnDownloaded)
+            {
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+        SetupEnvirnmentForBuidlerScene();
     }
 
     void SetupEnvirnmentForBuidlerScene()
