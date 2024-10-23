fb1239e8ad55834e4680e33b243871afa0e22b1d
WaqasAhmad-Unity <111582253+WaqasAhmad-Unity@users.noreply.github.com>
2024-10-23 13:48:37 +0500

    Remove the Counter while leaving the room
    https://www.notion.so/noborderz/PENPENZ-counting-shows-in-summit-and-app-stuck-256de118c1374062ab393353a4498048

--- Assets/Scripts/Emote Scripts/GamePlayUIHandler.cs
+++ Assets/Scripts/Emote Scripts/GamePlayUIHandler.cs
@@ -115,6 +115,10 @@ public class GamePlayUIHandler : MonoBehaviour
         {
             ConstantsHolder.IsSummitDomeWorld = false;
         }
+
+        if(ReferencesForGamePlay.instance != null && ReferencesForGamePlay.instance.XANAPartyCounterPanel.activeInHierarchy)
+            ReferencesForGamePlay.instance.XANAPartyCounterPanel.SetActive(false);
+
         GamePlayButtonEvents.inst.OnExitButtonClick();
     }
 
