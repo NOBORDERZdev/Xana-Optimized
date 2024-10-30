74a4cee6b3043b8c3084c79d204c2985a2467221
Ali Hamza <alihamzaunity@noborderz.com>
2024-10-30 13:29:33 +0500

    version revert

--- Assets/Scripts/MutiplayerController.cs
+++ Assets/Scripts/MutiplayerController.cs
@@ -69,7 +69,7 @@ namespace Photon.Pun.Demo.PunBasics
         /// <summary>
         /// This client's version number. Users are separated from each other by gameVersion (which allows you to make breaking changes).
         /// </summary>
-        string gameVersion = "XanaSummit241029";  // YYYYMMDD
+        string gameVersion = "XanaSummit241025";  // YYYYMMDD
 
         #endregion
         #region Multtisection Fields
