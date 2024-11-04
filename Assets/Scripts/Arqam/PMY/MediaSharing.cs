baf140d6e962de75a926cf4d8ad3f81e6cd2f64d
ArqamNoBorderz <arqam.javed@noborderz.com>
2024-11-04 12:44:26 +0500

    commit media sharing script

---
+++ Assets/Scripts/Arqam/PMY/MediaSharing.cs
@@ -0,0 +1,177 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class MediaSharing : MonoBehaviour
+{
+    public bool isPlayAtStart = false;
+    [Space(5)]
+    public RawImage screenImg;
+    public VideoPlayer player;
+
+    public string permissionCheck = "";
+
+    void Start()
+    {
+        BuilderEventManager.AfterWorldInstantiated += PlayAdminVideo;
+    }
+    private void OnDisable()
+    {
+        BuilderEventManager.AfterWorldInstantiated -= PlayAdminVideo;
+    }
+
+    private void PlayAdminVideo()
+    {
+        OnPickVideoFromGellery(256);
+    }
+
+
+    public void OnPickImageFromGellery(int maxSize)
+    {
+#if UNITY_IOS
+        if (permissionCheck == "false")
+        {
+            string url = MyNativeBindings.GetSettingsURL();
+            Debug.Log("the settings url is:" + url);
+            Application.OpenURL(url);
+        }
+        else
+        {
+            iOSCameraPermission.VerifyPermission(gameObject.name, "SampleCallback");
+        }
+
+        NativeGallery.Permission permission = NativeGallery.GetImageFromGallery((path) =>
+        {
+            if (path != null)
+            {
+                // Create Texture from selected image
+                Texture2D texture = NativeGallery.LoadImageAtPath(path, maxSize, false);
+                if (texture == null)
+                {
+                    Debug.Log("Couldn't load texture from " + path);
+                    return;
+                }
+                Debug.Log("OnPickGroupAvatarFromGellery path: " + path);
+                screenImg.texture = texture;
+            }
+        });
+        Debug.Log("Permission result: " + permission);
+
+#elif UNITY_ANDROID              
+        NativeGallery.Permission permission = NativeGallery.GetImageFromGallery((path) =>
+        {
+            if (path != null)
+            {
+                // Create Texture from selected image
+                Texture2D texture = NativeGallery.LoadImageAtPath(path, maxSize, false);
+                if (texture == null)
+                {
+                    Debug.Log("Couldn't load texture from " + path);
+                    return;
+                }
+                Debug.Log("OnPickGroupAvatarFromGellery path: " + path);
+                screenImg.texture = texture;
+            }
+        });
+        if (permission != NativeGallery.Permission.Granted)
+        {
+            using (var unityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+            using (AndroidJavaObject currentActivityObject = unityClass.GetStatic<AndroidJavaObject>("currentActivity"))
+            {
+                string packageName = currentActivityObject.Call<string>("getPackageName");
+
+                using (var uriClass = new AndroidJavaClass("android.net.Uri"))
+                using (AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("fromParts", "package", packageName, null))
+                using (var intentObject = new AndroidJavaObject("android.content.Intent", "android.settings.APPLICATION_DETAILS_SETTINGS", uriObject))
+                {
+                    intentObject.Call<AndroidJavaObject>("addCategory", "android.intent.category.DEFAULT");
+                    intentObject.Call<AndroidJavaObject>("setFlags", 0x10000000);
+                    currentActivityObject.Call("startActivity", intentObject);
+                }
+            }
+        }
+        Debug.Log("Permission result: " + permission);
+#endif
+
+    }
+
+    public void OnPickVideoFromGellery(int maxSize)
+    {
+#if UNITY_IOS
+        if (permissionCheck == "false")
+        {
+            string url = MyNativeBindings.GetSettingsURL();
+            Debug.Log("the settings url is:" + url);
+            Application.OpenURL(url);
+        }
+        else
+        {
+            iOSCameraPermission.VerifyPermission(gameObject.name, "SampleCallback");
+        }
+
+        NativeGallery.Permission permission = NativeGallery.GetVideoFromGallery((path) =>
+        {
+            if (path != null)
+            {
+                // Create Texture from selected image
+                player.url = path;
+                player.Play();
+            }
+        });
+        Debug.Log("Permission result: " + permission);
+
+#elif UNITY_ANDROID              
+        NativeGallery.Permission permission = NativeGallery.GetVideoFromGallery((path) =>
+        {
+            if (path != null)
+            {
+                // Create Texture from selected image
+                player.url = path;
+                player.Play();
+            }
+        });
+        if (permission != NativeGallery.Permission.Granted)
+        {
+            using (var unityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+            using (AndroidJavaObject currentActivityObject = unityClass.GetStatic<AndroidJavaObject>("currentActivity"))
+            {
+                string packageName = currentActivityObject.Call<string>("getPackageName");
+
+                using (var uriClass = new AndroidJavaClass("android.net.Uri"))
+                using (AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("fromParts", "package", packageName, null))
+                using (var intentObject = new AndroidJavaObject("android.content.Intent", "android.settings.APPLICATION_DETAILS_SETTINGS", uriObject))
+                {
+                    intentObject.Call<AndroidJavaObject>("addCategory", "android.intent.category.DEFAULT");
+                    intentObject.Call<AndroidJavaObject>("setFlags", 0x10000000);
+                    currentActivityObject.Call("startActivity", intentObject);
+                }
+            }
+        }
+        Debug.Log("Permission result: " + permission);
+#endif
+
+    }
+
+    public void SampleCallback(string permissionWasGranted)
+    {
+        Debug.Log("Callback.permissionWasGranted = " + permissionWasGranted);
+
+        if (permissionWasGranted == "true")
+        {
+            // You can now use the device camera.
+        }
+        else
+        {
+            permissionCheck = permissionWasGranted;
+
+            // permission denied, no access should be visible, when activated when requested permission
+            return;
+
+            // You cannot use the device camera.  You may want to display a message to the user
+            // about changing the camera permission in the Settings app.
+            // You may want to re-enable the button to display the Settings message again.
+        }
+    }
+}
