a25fb4f3f500c1738a90c09d6ed962a486a4398b
ArqamNoBorderz <arqam.javed@noborderz.com>
2024-11-06 12:11:19 +0500

    updated ui and logic

--- Assets/Scripts/Arqam/PMY/Post&Comment/CommentUIManager.cs
+++ Assets/Scripts/Arqam/PMY/Post&Comment/CommentUIManager.cs
@@ -2,14 +2,13 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System.Collections;
-using static System.Net.Mime.MediaTypeNames;
 
 public class CommentUIManager : MonoBehaviour
 {
     [SerializeField]
     private TextMeshProUGUI UserNameTxt;
     [SerializeField]
-    private TextMeshProUGUI commentText;
+    private TMP_InputField commentText;
     [SerializeField]
     private Button seeMoreButton;
     [SerializeField]
@@ -18,37 +17,47 @@ public class CommentUIManager : MonoBehaviour
     private TextMeshProUGUI likeText;
     [SerializeField]
     private TextMeshProUGUI dislikeText;
-    [Space(5)][SerializeField]
+    [Space(5)]
+    [SerializeField]
     private ContentSizeFitter txtSizeFitter;
     [SerializeField]
     private ContentSizeFitter bodySizeFitter;
     [SerializeField]
     private ContentSizeFitter uiSizeFitter;
-    //[SerializeField][TextArea]
+    [SerializeField]
+    private GameObject replySection;
+    [SerializeField]
+    private TMP_InputField replyInputField;
+    [SerializeField]
+    private GameObject childCommentUI;
+
     private string fullComment;
     private int totalLike = 0;
     private int totalDislike = 0;
 
     private void OnEnable()
     {
-        commentText.text = UserNameTxt.text = likeText.text = dislikeText.text = "";
+         commentText.text = UserNameTxt.text = likeText.text = dislikeText.text = "";
     }
 
-    public void SetComment(string userName,string comment)
+    public void SetComment(string userName, string comment)
     {
         UserNameTxt.text = userName;
+        commentText.text = comment;
         fullComment = comment;
-        if (comment.Length > previewLength)
+
+        commentText.textComponent.ForceMeshUpdate();
+        int lineCount = commentText.textComponent.textInfo.lineCount;
+        Debug.LogError("lineCount: " + lineCount);
+        if (lineCount > 2)
         {
-            commentText.text = comment.Substring(0, previewLength) + "...";
+            int characterCount = commentText.textComponent.textInfo.lineInfo[0].characterCount + (commentText.textComponent.textInfo.lineInfo[1].characterCount - 15);
+            Debug.LogError("characterCount2: " + characterCount);
+            commentText.text = comment.Substring(0, characterCount) + "...";
             seeMoreButton.gameObject.SetActive(true);
         }
-        else
-        {
-            commentText.text = comment;
-            seeMoreButton.gameObject.SetActive(false);
-        }
     }
+
 
     public void ShowFullComment()
     {
@@ -76,10 +85,38 @@ public class CommentUIManager : MonoBehaviour
         dislikeText.text = totalDislike.ToString();
     }
 
+    public void ClickOnReply()
+    {
+        replySection.SetActive(true);
+    }
+
+    public void ClickOnCancel()
+    {
+        replySection.SetActive(false);
+    }
+
+    public void SendReply()
+    {
+        if (!replyInputField.text.IsNullOrEmpty())
+        {
+            replySection.SetActive(false);
+            GameObject child = Instantiate(childCommentUI);
+            child.transform.SetParent(transform, false);
+            child.GetComponent<CommentUIManager>().SetComment("Xana PMY", replyInputField.text);
+            replyInputField.text = "";
+        }
+        else
+        {
+            Debug.LogError("Please Enter something");
+        }
+    }
+
     private void SetSizeFitter()
     {
         txtSizeFitter.verticalFit = ContentSizeFitter.FitMode.PreferredSize;
         bodySizeFitter.verticalFit = ContentSizeFitter.FitMode.PreferredSize;
         uiSizeFitter.verticalFit = ContentSizeFitter.FitMode.PreferredSize;
     }
+
+
 }
